f505cb7106bc5f3ea0d716373bd023c84c6732db
xjf1766369541 <1766369541@qq.com>
2020-04-16 21:13:59 +0800

    第二次提交

--- src/com_bigdata/HelloAA.java
+++ src/com_bigdata/HelloAA.java
@@ -3,7 +3,7 @@ package com_bigdata;
 public class HelloAA {
 	 public static void main(String[] args) {
 			
-		   System.out.println("今天开发了菜单管理功能,真烦躁");
+		   System.out.println("tomhello");
 		   
 		   
 	}
