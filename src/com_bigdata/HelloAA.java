26fc0498a685f3b3a5645f4f35dced8f5b803da5
xjf1766369541 <1766369541@qq.com>
2020-04-16 21:05:31 +0800

    开起了烦躁的开关

--- src/com_bigdata/HelloAA.java
+++ src/com_bigdata/HelloAA.java
@@ -3,8 +3,7 @@ package com_bigdata;
 public class HelloAA {
 	 public static void main(String[] args) {
 			
-		   System.out.println("今天开发了菜单管理功能");
-		   
+		   System.out.println("今天开发了菜单管理功能,真烦躁");
 		   
 		   
 	}
